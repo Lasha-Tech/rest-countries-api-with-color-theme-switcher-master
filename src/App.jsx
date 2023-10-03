63418df7a5db98a55b042e4eae4b852c5fb8aa2b
Lasha Efremidze <lashaefremidze1@gmail.com>
2023-10-03 18:47:05 +0400

    docs: create country component for mobile responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,177 @@
-import { useState } from 'react'
-import styled, { css } from 'styled-components'
+import { useState } from "react";
+import styled, { css } from "styled-components";
+import Country from "./Country";
 
 function App() {
-  const [day, setDay] = useState(true)
-  const [region, setRegion] = useState('Filter by Region')
-  const [list, setList] = useState(false)
+  const [day, setDay] = useState(true);
+  const [region, setRegion] = useState("Filter by Region");
+  const [list, setList] = useState(false);
 
   return (
-    <Home day={day}>
+    <div
+      className="App"
+      style={{ backgroundColor: day ? "#FAFAFA" : "#202C36" }}
+    >
       {/* Header  */}
-      <Header day={day}>
-        <p className='title'>Where in the world?</p>
+      <div
+        className="header"
+        style={{ backgroundColor: day ? "#FFF" : "#2B3844" }}
+      >
+        <p className="title" style={{ color: day ? "#111517" : "#FFF" }}>
+          Where in the world?
+        </p>
         <div className="day-night-div" onClick={() => setDay(!day)}>
-          <svg className='moon' xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-            <path fillRule="evenodd" clipRule="evenodd" d="M10.8426 11.052C7.73486 11.052 5.21543 8.74226 5.21543 5.89457C5.21543 4.82024 5.57343 3.82526 6.18514 3C3.75229 3.75612 2 5.86498 2 8.35045C2 11.4708 4.75943 14 8.16286 14C10.8743 14 13.1757 12.3945 14 10.1636C13.1 10.7238 12.0129 11.052 10.8426 11.052Z"/>
+          <svg
+            className="moon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              style={{ stroke: day ? "#111517" : "none" }}
+              d="M10.8426 11.052C7.73486 11.052 5.21543 8.74226 5.21543 5.89457C5.21543 4.82024 5.57343 3.82526 6.18514 3C3.75229 3.75612 2 5.86498 2 8.35045C2 11.4708 4.75943 14 8.16286 14C10.8743 14 13.1757 12.3945 14 10.1636C13.1 10.7238 12.0129 11.052 10.8426 11.052Z"
+            />
           </svg>
-          <p className='daynight'>Dark Mode</p>
+          <p className="daynight" style={{ color: day ? "#111517" : "#FFF" }}>
+            Dark Mode
+          </p>
         </div>
-      </Header>
+      </div>
 
-    {/* Main  */}
-    <Main day={day} list={list}>
-      {/* Search  */}
-      <div className="input-div">
+      {/* Main  */}
+      <div className="main">
+        {/* Search  */}
+        <div
+          className="input-div"
+          style={{ backgroundColor: day ? "#FFF" : "#2B3844" }}
+        >
           <div className="search-div">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none">
-              <path fillRule="evenodd" clipRule="evenodd" d="M11.1111 9.77778H10.4L10.1333 9.51111C11.0222 8.53333 11.5556 7.2 11.5556 5.77778C11.5556 2.57778 8.97778 0 5.77778 0C2.57778 0 0 2.57778 0 5.77778C0 8.97778 2.57778 11.5556 5.77778 11.5556C7.2 11.5556 8.53333 11.0222 9.51111 10.1333L9.77778 10.4V11.1111L14.2222 15.5556L15.5556 14.2222L11.1111 9.77778ZM5.77778 9.77778C3.55556 9.77778 1.77778 8 1.77778 5.77778C1.77778 3.55556 3.55556 1.77778 5.77778 1.77778C8 1.77778 9.77778 3.55556 9.77778 5.77778C9.77778 8 8 9.77778 5.77778 9.77778Z"/>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="none"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                style={{ fill: day ? "#B2B2B2" : "#FFF" }}
+                d="M11.1111 9.77778H10.4L10.1333 9.51111C11.0222 8.53333 11.5556 7.2 11.5556 5.77778C11.5556 2.57778 8.97778 0 5.77778 0C2.57778 0 0 2.57778 0 5.77778C0 8.97778 2.57778 11.5556 5.77778 11.5556C7.2 11.5556 8.53333 11.0222 9.51111 10.1333L9.77778 10.4V11.1111L14.2222 15.5556L15.5556 14.2222L11.1111 9.77778ZM5.77778 9.77778C3.55556 9.77778 1.77778 8 1.77778 5.77778C1.77778 3.55556 3.55556 1.77778 5.77778 1.77778C8 1.77778 9.77778 3.55556 9.77778 5.77778C9.77778 8 8 9.77778 5.77778 9.77778Z"
+              />
             </svg>
           </div>
-          <input type="text" placeholder='Search for a country…'/>
-      </div>
+          <Input day={day} type="text" placeholder="Search for a country…"/>
+    </div>
 
-      {/* Filter  */}
-      <div className="filter-div">
-        <div className="filter-container">
-          <div className="filter">
-            <p className='region'>{region}</p>
-            <svg onClick={() => setList(!list)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
-              <path fillRule="evenodd" clipRule="evenodd" d="M7.875 2.875L5 5.75L2.125 2.875L1.25 3.75L5 7.5L8.75 3.75L7.875 2.875Z"/>
-            </svg>
+        {/* Filter  */}
+        <div className="filter-div">
+          <div className="filter-container">
+            <div
+              className="filter"
+              style={{ backgroundColor: day ? "#FFF" : "#2B3844" }}
+            >
+              <p className="region" style={{ color: day ? "#111517" : "#FFF" }}>
+                {region}
+              </p>
+              <svg
+                onClick={() => setList(!list)}
+                style={{ fill: day ? "black" : "white" }}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 10 10"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M7.875 2.875L5 5.75L2.125 2.875L1.25 3.75L5 7.5L8.75 3.75L7.875 2.875Z"
+                />
+              </svg>
+            </div>
+
+            {/* Region List  */}
+            <RegionList day={day} list={list}>
+              <p
+                className="region-list-item"
+                style={{ color: day ? "#111517" : "#FFF" }}
+                onClick={() => {
+                  setRegion("Africa"), setList(false);
+                }}
+              >
+                Africa
+              </p>
+              <p
+                className="region-list-item"
+                style={{ color: day ? "#111517" : "#FFF" }}
+                onClick={() => {
+                  setRegion("America"), setList(false);
+                }}
+              >
+                America
+              </p>
+              <p
+                className="region-list-item"
+                style={{ color: day ? "#111517" : "#FFF" }}
+                onClick={() => {
+                  setRegion("Asia"), setList(false);
+                }}
+              >
+                Asia
+              </p>
+              <p
+                className="region-list-item"
+                style={{ color: day ? "#111517" : "#FFF" }}
+                onClick={() => {
+                  setRegion("Europe"), setList(false);
+                }}
+              >
+                Europe
+              </p>
+              <p
+                className="region-list-item"
+                style={{ color: day ? "#111517" : "#FFF" }}
+                onClick={() => {
+                  setRegion("Oceania"), setList(false);
+                }}
+              >
+                Oceania
+              </p>
+              </RegionList>
+          </div>
           </div>
 
-          {/* Region List  */}
-          <div className="region-list">
-            <p onClick={() => {setRegion('Africa'), setList(false)}}>Africa</p>
-            <p onClick={() => {setRegion('America'), setList(false)}}>America</p>
-            <p onClick={() => {setRegion('Asia'), setList(false)}}>Asia</p>
-            <p onClick={() => {setRegion('Europe'), setList(false)}}>Europe</p>
-            <p onClick={() => {setRegion('Oceania'), setList(false)}}>Oceania</p>
-          </div>
-        </div>
-      </div>
+        {/* Countries */}
+        <div className="countries-container">
+          <div
+            className="country"
+            style={{ backgroundColor: day ? "#FFF" : "#2B3844" }}
+          >
+            <img
+              src="https://s3-alpha-sig.figma.com/img/dfb7/6a9f/4829ccead8b50a47638ac3648f0aec0c?Expires=1697414400&Signature=WBx849ebsKTHmeo66~V3lWXhJ5FTPy14anRNU~Aw47Q6MJhkriYm6qLUop3c85VcJGzwiMjrD-ml8vZuCYjrOrc~3YtNvrEWtNp10~Y3FFgEi7m5XEStmgfVr3ebljeUQZR-LeOVnU6jP~YD2WCbfqKd6K2addTvGQAKhz6sIvYq9dJlkbVV1Cy16W9gwEtC3XD97zm6O8KNlCzmMzfQKzwfMadHS4cqqLw7kZCZyulAd1hvKP8cOu~ZX3fXfAmcrXNJii9pzwbdAO0X0GSDRrJJQVwRe1YDymSzHQCNYLxOgW0EcEpjbLY9g1oqL3qHb3l2S-ngMQimhsUYGtCI0A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt="flag"
+            />
 
-      {/* Countries  */}
-      <div className="countries-container">
-        <div className="country">
-          <img src="https://s3-alpha-sig.figma.com/img/dfb7/6a9f/4829ccead8b50a47638ac3648f0aec0c?Expires=1697414400&Signature=WBx849ebsKTHmeo66~V3lWXhJ5FTPy14anRNU~Aw47Q6MJhkriYm6qLUop3c85VcJGzwiMjrD-ml8vZuCYjrOrc~3YtNvrEWtNp10~Y3FFgEi7m5XEStmgfVr3ebljeUQZR-LeOVnU6jP~YD2WCbfqKd6K2addTvGQAKhz6sIvYq9dJlkbVV1Cy16W9gwEtC3XD97zm6O8KNlCzmMzfQKzwfMadHS4cqqLw7kZCZyulAd1hvKP8cOu~ZX3fXfAmcrXNJii9pzwbdAO0X0GSDRrJJQVwRe1YDymSzHQCNYLxOgW0EcEpjbLY9g1oqL3qHb3l2S-ngMQimhsUYGtCI0A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="flag"/>
-          <div className="about-div">
             {/* Country Information  */}
-            <div className='about'>
-              <h1>United States Of America</h1>
-              <div>
-                <p className='about-title'>Population:</p>
-                <p className='about-information'>323,947,000</p>
-              </div>
-              <div>
-                <p className='about-title'>Region:</p>
-                <p className='about-information'>Americas</p>
-              </div>
-              <div>
-                <p className='about-title'>Capital:</p>
-                <p className='about-information'>Washington, D.C.</p>
+            <div className="about-div">             
+              <div className="about">
+                <h1 style={{ color: day ? "#111517" : "#FFF" }}>
+                  United States Of America
+                </h1>
+                <CountryInfoDiv>
+                  <CountryInfoTitle day={day}>Population:</CountryInfoTitle>
+                  <CountryInfo day={day}>cs</CountryInfo>
+                </CountryInfoDiv>
+                
+                <CountryInfoDiv>
+                  <CountryInfoTitle day={day}>Region:</CountryInfoTitle>
+                  <CountryInfo day={day}>acas</CountryInfo>
+                </CountryInfoDiv>
+                
+                <CountryInfoDiv>
+                  <CountryInfoTitle day={day}>Capital:</CountryInfoTitle>
+                  <CountryInfo day={day}>casacs</CountryInfo>
+                </CountryInfoDiv>
               </div>
             </div>
           </div>
@@ -78,250 +179,68 @@ function App() {
       </div>
 
 
-    </Main>
-    </Home>
-  )
+    {/* <Country day={day}/> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
 
-const Home = styled.div(
+// Styled Components
+const Input = styled.input(
   (props) => css`
-  @media (min-width: 375px) {
-    width: 100%;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: ${props.day? '#FAFAFA': '#202C36'};
-    overflow: hidden;
-  }   
+    @media (min-width: 375px) {
+      border: none;
+      outline: none;
+      width: 100%;
+      background: ${props.day ? "#FFF" : "#2B3844"};
+      color: ${props.day ? "#111517" : "#FFF"};
+      font-family: "Nunito Sans";
+      font-weight: 600;
+      font-size: 12px;
+      line-height: 20px;
+      &::placeholder {
+        color: ${props.day ? "#C4C4C4" : "#FFF"};
+        font-weight: 400;
+      }
+    }
   `
-)
-const Header = styled.div(
+);
+const RegionList = styled.div(
   (props) => css`
     @media (min-width: 375px) {
       width: 100%;
-      height: 80px;
-      background: ${props.day? '#FFF': '#2B3844'};
-      box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.06);
-      display: flex;
-      align-items: center;
+      height: 144px;
+      padding: 16px 24px;
+      border-radius: 5px;
+      background: ${props.day ? "#FFF" : "#2B3844"};
+      box-shadow: 0px 2px 9px 0px rgba(0, 0, 0, 0.05);
+      display: ${props.list ? "flex" : "none"};
+      flex-direction: column;
+      gap: 8px;
       justify-content: space-between;
-      padding: 0 16px;
-
-      .title {
-        color: ${props.day? '#111517': '#FFF'};
-        font-family: 'Nunito Sans';
-        font-size: 14px;
-        font-weight: 800;
-        line-height: 20px;
-      }
-
-      .day-night-div {
-        display: flex;
-        align-items: center;
-        gap: 8px;
-        &:hover {
-          cursor: pointer;
-        }
-      }
-
-      .moon path {
-        fill: #FFF;
-        stroke: ${props.day? '#111517': 'none'};
-      }
-
-      .daynight {
-        color: ${props.day? '#111517': '#FFF'};
-        font-family: 'Nunito Sans';
-        font-size: 12px;
-        font-weight: 600;
-      }
     }
   `
-)
-const Main = styled.div(
+);
+const CountryInfoDiv = styled.div(
   (props) => css`
-  @media (min-width: 375px) {
-    width: 100%;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 24px 16px 65px;
-
-  .input-div {
-    width: 100%;
-    height: 48px;
-    padding: 16px 32px;
-    display: flex;
-    gap: 26px;
-    border-radius: 5px;
-    background: ${props.day? '#FFF': '#2B3844'};
-    box-shadow: 0px 2px 9px 0px rgba(0, 0, 0, 0.05);
-  }
-
-  .search-div svg {
-    width: 16px;
-    height: 16px;
-  }
-
-  .search-div > svg > path {
-    fill: ${props.day? '#B2B2B2': '#FFF'};
-  }
-
-  .input-div input {
-    border: none;
-    outline: none;
-    width: 100%;
-    background: ${props.day? '#FFF': '#2B3844'};
-    color: ${props.day? '#111517': '#FFF'};
-    font-family: 'Nunito Sans';
-    font-weight: 600;
-    font-size: 12px;
-    line-height: 20px;
-    &::placeholder {
-      color: ${props.day? '#C4C4C4': '#FFF'};
-      font-weight: 400;
+    @media (min-width: 375px) {
+      width: 100%;
+      display: flex;
+      gap: 4px;
+      align-items: center;
     }
-  }
-
-  .filter-div {
-    width: 100%;
-    display: flex;
-    align-items: flex-start;
-  }
-
-  .filter-container {
-    margin-top: 40px;
-    width: 65%;
-    height: max-content;
-    position: relative;
-    z-index: 2;
-  }
-
-  .filter {
-    width: 100%;
-    height: 48px;
-    border-radius: 5px;
-    background: ${props.day? '#FFF': '#2B3844'};
-    box-shadow: 0px 2px 9px 0px rgba(0, 0, 0, 0.05);
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 14px 19px 14px 24px;
-    margin-bottom: 4px;
-  }
-
-  .region {
-    color: ${props.day? '#111517': '#FFF'};
-    font-family: 'Nunito Sans';
-    font-size: 12px;
-    font-weight: 600;
-    line-height: 20px;
-  }
-
-  .filter svg {
-    width: 10px;
-    height: 10px;
-    fill: ${props.day? 'black': 'white'};
-  }
-
-  .region-list {
-    width: 100%;
-    height: 144px;
-    padding: 16px 24px;
-    border-radius: 5px;
-    background: ${props.day? '#FFF': '#2B3844'};
-    box-shadow: 0px 2px 9px 0px rgba(0, 0, 0, 0.05);
-    display: ${props.list? 'flex': 'none'};
-    flex-direction: column;
-    gap: 8px;
-    justify-content: space-between;
-  }
-
-  .region-list p {
-    color: ${props.day? '#111517': '#FFF'};
-    font-family: 'Nunito Sans';
-    font-size: 12px;
-    line-height: 16px;
-    font-weight: 600;
-  }
-
-  .countries-container {
-    width: 90%;
-    height: max-content;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 40px;
-    position: absolute;
-    z-index: 0;
-    top: 285px;
-  }
-
-  .country {
-    width: 80%;
-    height: 325px;
-    border-radius: 5px;
-    background: ${props.day? '#FFF': '#2B3844'};
-    box-shadow: 0px 0px 7px 2px rgba(0, 0, 0, 0.03);
-    overflow: hidden;
-  }
-
-  .country img {
-    width: 100%;
-    height: 140px;
-    border-radius: 5px 5px 0 0;
-  }
-
-  .about-div {
-    width: 100%;
-    height: 57%;
-  }
-
-  .about {
-    margin-top: 24px;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    align-items: flex-start;
-    padding: 0 24px;
-  } 
-
-  .about h1 {
-    text-align: left;
-    color: ${props.day? '#111517': '#FFF'};
-    font-family: 'Nunito Sans';
-    font-size: 18px;
-    font-weight: 800;
-    line-height: 26px;
-    margin-bottom: 8px;
-  }
-
-  .about div {
-    width: 100%;
-    display: flex;
-    gap: 4px;
-    align-items: center;
-  }
-
-  .about-title {
-    color: ${props.day? '#111517': '#FFF'};
+  `
+);
+const CountryInfoTitle = styled.p(
+  (props) => css`
     font-family: 'Nunito Sans';
     font-size: 14px;
     font-weight: 600;
     line-height: 16px;
-  }
-
-  .about-information {
-    color: ${props.day? '#111517': '#FFF'};
-    font-family: 'Nunito Sans';
-    font-size: 14px;
-    line-height: 16px;
-  }
-}
-`
-)
+    color: ${props.day ? "#111517" : "#FFF"};
+  `
+);
+const CountryInfo = styled(CountryInfoTitle)`
+  font-weight: 400;
+`;
---
+++ src/Country.jsx
@@ -0,0 +1,127 @@
+import styled, { css } from "styled-components";
+
+const Country = ({ day }) => {
+  return (
+    <div className="country-home">
+      {/* Back Button  */}
+      <div
+        className="back"
+        style={{ backgroundColor: day ? "#FFF" : "#2B3844" }}
+      >
+        <svg
+          style={{ fill: day ? "#111517" : "#FFF" }}
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z"
+          />
+        </svg>
+        <p style={{ color: day ? "#111517" : "#FFF" }}>Back</p>
+      </div>
+
+      {/* Flag  */}
+      <img
+        className="country-flag"
+        src="https://s3-alpha-sig.figma.com/img/dfb7/6a9f/4829ccead8b50a47638ac3648f0aec0c?Expires=1697414400&Signature=WBx849ebsKTHmeo66~V3lWXhJ5FTPy14anRNU~Aw47Q6MJhkriYm6qLUop3c85VcJGzwiMjrD-ml8vZuCYjrOrc~3YtNvrEWtNp10~Y3FFgEi7m5XEStmgfVr3ebljeUQZR-LeOVnU6jP~YD2WCbfqKd6K2addTvGQAKhz6sIvYq9dJlkbVV1Cy16W9gwEtC3XD97zm6O8KNlCzmMzfQKzwfMadHS4cqqLw7kZCZyulAd1hvKP8cOu~ZX3fXfAmcrXNJii9pzwbdAO0X0GSDRrJJQVwRe1YDymSzHQCNYLxOgW0EcEpjbLY9g1oqL3qHb3l2S-ngMQimhsUYGtCI0A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+        alt="flag"
+      />
+      {/* Name  */}
+      <p className="country-name" style={{ color: day ? "#111517" : "#FFF" }}>
+        United States Of America
+      </p>
+      {/* Country Information */}
+      {/* Header  */}
+      <div className="country-header">
+        <CountryInfoDiv>
+          <CountryInfoTitle day={day}>Native Name:</CountryInfoTitle>
+          <CountryInfo></CountryInfo>
+        </CountryInfoDiv>
+
+        <CountryInfoDiv>
+          <CountryInfoTitle day={day}>Population:</CountryInfoTitle>
+          <CountryInfo></CountryInfo>
+        </CountryInfoDiv>
+
+        <CountryInfoDiv>
+          <CountryInfoTitle day={day}>Region:</CountryInfoTitle>
+          <CountryInfo></CountryInfo>
+        </CountryInfoDiv>
+
+        <CountryInfoDiv>
+          <CountryInfoTitle day={day}>Sub Region:</CountryInfoTitle>
+          <CountryInfo></CountryInfo>
+        </CountryInfoDiv>
+
+        <CountryInfoDiv>
+          <CountryInfoTitle day={day}>Capital:</CountryInfoTitle>
+          <CountryInfo></CountryInfo>
+        </CountryInfoDiv>
+      </div>
+
+      {/* Main  */}
+      <div className="country-main">
+        <CountryInfoDiv>
+          <CountryInfoTitle day={day}>Top Level Domain:</CountryInfoTitle>
+          <CountryInfo></CountryInfo>
+        </CountryInfoDiv>
+
+        <CountryInfoDiv>
+          <CountryInfoTitle day={day}>Currencies:</CountryInfoTitle>
+          <CountryInfo></CountryInfo>
+        </CountryInfoDiv>
+
+        <CountryInfoDiv>
+          <CountryInfoTitle day={day}>Languages:</CountryInfoTitle>
+          <CountryInfo></CountryInfo>
+        </CountryInfoDiv>
+      </div>
+
+      {/* Footer  */}
+      <div className="country-footer">
+        <h1
+          className="border-countries-title"
+          style={{ color: day ? "#111517" : "#FFF" }}
+        >
+          Border Countries:
+        </h1>
+
+        <div className="border-countries-container">
+          <div
+            className="border-countries"
+            style={{ backgroundColor: day ? "#FFF" : "#2B3844" }}
+          >
+            <p style={{ color: day ? "#111517" : "#FFF" }}>Georgia</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Country;
+
+const CountryInfoDiv = styled.div(
+  (props) => css`
+    @media (min-width: 375px) {
+      display: flex;
+      align-items: center;
+      gap: 4px;
+    }
+  `
+);
+const CountryInfoTitle = styled.p(
+  (props) => css`
+    font-family: "Nunito Sans";
+    font-weight: 600;
+    line-height: 32px;
+    color: ${props.day ? "#111517" : "#FFF"};
+  `
+);
+const CountryInfo = styled(CountryInfoTitle)`
+  font-weight: 400;
+`;
